7975a1e4fb61ec657cccfa12ffb0f3894fac2e9d
llobato <louplelpro@gmail.com>
2023-02-10 11:58:03 +0100

    text centrer landing page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,7 @@
             </svg>
 
         </div>
-        <div class="max-w-3xl flex flex-col items-center gap-6">
+        <div class="max-w-3xl flex flex-col items-center gap-6 mx-auto">
             <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd"
                     d="M20 3.47396e-07L0 20L20 40L40 20L20 3.47396e-07ZM24.2969 24.1406C26.5837 21.8538 26.5837 18.1462 24.2969 15.8594C22.0101 13.5726 18.3024 13.5726 16.0156 15.8594C13.7288 18.1462 13.7288 21.8538 16.0156 24.1406C18.3024 26.4274 22.0101 26.4274 24.2969 24.1406Z"
@@ -51,7 +51,7 @@
 
 
         </div>
-        <div class=" flex items-end pb-11 w-80">
+        <div class=" flex items-end pb-11">
             <svg class="mb-36 hidden xl:block" width="208" height="208" viewBox="0 0 303 304" fill="none"
                 xmlns="http://www.w3.org/2000/svg">
                 <circle cx="151.5" cy="152" r="151.5" fill="black" />
