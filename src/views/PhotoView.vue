a85de26b40af66c8039fe4b2bd5105eb119091ce
llobato <louplelpro@gmail.com>
2023-03-16 21:52:43 +0100

    photo responsive 2

--- src/views/PhotoView.vue
+++ src/views/PhotoView.vue
@@ -69,9 +69,9 @@ function screenCheck() {
 
 <template >
     <!--<div class="flex w-full justify-center">
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                </div>-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            </div>-->
     <section data-section-name="sec1" class="w-full">
-        <div class="flex w-4/5 justify-around items-center h-[100vh]  mx-auto">
+        <div class="flex w-4/5 justify-around items-center md:h-[100vh]  mx-auto">
             <div class="flex items-center w-full justify-center" aria-controls="ban" :aria-expanded="hoverban"
                 @mouseenter="hoverban = !hoverban" @mouseleave="hoverban = !hoverban">
 
@@ -95,19 +95,20 @@ function screenCheck() {
                     <circle cx="250.006" cy="104.211" r="104" fill="black" />
                     <circle cx="282.006" cy="268.211" r="136" fill="black" />
                 </svg>
-
-
             </div>
-
-
-
         </div>
     </section>
     <section data-section-name="sec1" class="w-full">
-        <div class="flex w-4/5 justify-around items-center h-[100vh]  mx-auto">
+        <div class="flex w-4/5 justify-around items-center md:h-[100vh]  mx-auto">
             <div class="flex items-center w-full justify-center" aria-controls="ban" :aria-expanded="hoverban"
                 @mouseenter="hoverban = !hoverban" @mouseleave="hoverban = !hoverban">
-
+                <svg width="419" height="405" viewBox="0 0 419 405" fill="none" xmlns="http://www.w3.org/2000/svg "
+                    class="motion-reduce:transition-transform motion-safe:duration-500 mt-9 hidden w-[420px] xl:  xl:block "
+                    :class="['', hoverban ? ' opacity-100, opacity-0 ' : '']">
+                    <circle cx="125.006" cy="217.211" r="125" fill="black" />
+                    <circle cx="250.006" cy="104.211" r="104" fill="black" />
+                    <circle cx="282.006" cy="268.211" r="136" fill="black" />
+                </svg>
                 <div class="flex items-center justify-end">
                     <p class="mr-4 md:mr-[-10vh] absolute xl:text-[5vw] text-6xl font-Chopper text-white md:mix-blend-difference z-20 text-right shadowtext motion-reduce:transition-transform motion-safe:duration-500 w-[420px]"
                         :class="['', hoverban ? ' opacity-100, opacity-0 ' : '']">
@@ -121,23 +122,14 @@ function screenCheck() {
                         :class="['', hoverban ? ' scale-95, scale-105 ' : '']">
                 </div>
 
-                <svg width="419" height="405" viewBox="0 0 419 405" fill="none" xmlns="http://www.w3.org/2000/svg "
-                    class="motion-reduce:transition-transform motion-safe:duration-500 mt-9 hidden w-[420px] xl:  xl:block "
-                    :class="['', hoverban ? ' opacity-100, opacity-0 ' : '']">
-                    <circle cx="125.006" cy="217.211" r="125" fill="black" />
-                    <circle cx="250.006" cy="104.211" r="104" fill="black" />
-                    <circle cx="282.006" cy="268.211" r="136" fill="black" />
-                </svg>
+
 
 
             </div>
-
-
-
         </div>
     </section>
     <section data-section-name="sec1" class="w-full">
-        <div class="flex w-4/5 justify-around items-center h-[100vh]  mx-auto">
+        <div class="flex w-4/5 justify-around items-center md:h-[100vh]  mx-auto">
             <div class="flex items-center w-full justify-center" aria-controls="ban" :aria-expanded="hoverban"
                 @mouseenter="hoverban = !hoverban" @mouseleave="hoverban = !hoverban">
 
@@ -161,12 +153,7 @@ function screenCheck() {
                     <circle cx="250.006" cy="104.211" r="104" fill="black" />
                     <circle cx="282.006" cy="268.211" r="136" fill="black" />
                 </svg>
-
-
             </div>
-
-
-
         </div>
     </section>
 </template>
