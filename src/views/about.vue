c0d9c5e3fd724344233a0067bcc308f4ddde894e
llobato <louplelpro@gmail.com>
2023-03-19 16:30:47 +0100

    A propos

---
+++ src/views/about.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="h-[100%] mb-32">
+        <h1 class=" font-Chopper text-[5vw] text-black text-center mt-36">
+            A Propos
+        </h1>
+        <p class="font-Chopper text-black md:text-2xl w-2/4 text-center mx-auto">
+            Je me présente, je m'appelle Loup Lobato et je suis étudiant en BUT Métiers du Multimédia et de l'Internet. Mon
+            objectif professionnel est de devenir un designer web et print qualifié, tout en cultivant ma passion pour la
+            photographie. Mon intérêt pour l'art se reflète dans mes centres d'intérêt, en particulier la photographie, le
+            graphisme et la vidéo. En tant que photographe amateur, j'apprécie l'art de capturer des moments et faire
+            ressortir
+            une atmosphère souvant mélancolique et mystique. Mon ambition est d'améliorer mes compétences artistiques et
+            techniques afin de créer des designs innovants et créatifs dans le monde du web et du print.
+        </p>
+    </div>
+</template>
+<style></style>
+<script></script>
